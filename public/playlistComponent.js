8a30fe664a63a3e43c008108515dbca949259615
Donald Brunais <donb1991@hotmail.com>
2015-12-08 10:38:19 -0800

    added state to search and pace

--- public/playlistComponent.js
+++ public/playlistComponent.js
@@ -1,18 +1,35 @@
 var Playlist = React.createClass({
+  getInitialState: function() {
+    return {searchValue: '', pace: ''};
+  },
+
+  handleChange: function(event) {
+    var nextState = {};
+    nextState[event.target.name] = event.target.value;
+    this.setState(nextState);
+  },
+  getTracks: function() {
+    $.get("http://localhost:3000/search").done(function(data) {
+      console.log(data);
+    });
+  },
   render: function() {
     return <div>
       <div className="row">
-        <div className="large-6 columns">
+        <div className="large-8 columns">
           <div className="row">
-            <div className="large-10 columns">
-              <input type="text" placeholder="Search" />
+            <div className="large-8 columns">
+              <input type="text" name='searchValue' placeholder="Search" value={this.state.searchValue} onChange={this.handleChange}/>
             </div>
             <div className="large-2 columns">
-              <input type="text" name="name" placeholder="Pace" />
+              <input type="text" name="pace" placeholder="Pace" value={this.state.pace} onChange={this.handleChange}/>
+            </div>
+            <div className="large-2 columns">
+              <button className="button" type='button' name='button' onClick={this.getTracks}>Search</button>
             </div>
           </div>
         </div>
-        <div className="large-6 columns">
+        <div className="large-4 columns">
           <div className="large-4 columns large-offset-8">
             <button className="button" type="button" name="button">Export to Spotify</button>
           </div>
