48dbb9d691e91d35ae6e093c758f0005ba900356
Donald Brunais <donb1991@hotmail.com>
2015-12-08 10:17:27 -0800

    created static response for search results

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
 var db = require('../models');
+var request = require('request');
 
 app.get('/', function(req, res) {
   res.render('buildPlaylist.ejs');
+});
+
+app.get('/search', function(req, res) {
+  request.get('http://developer.echonest.com/api/v4/song/profile?api_key=' + process.env.ECHONESTAPIKEY + '&id=SOUJWUH13E89D89DED&bucket=audio_summary', function(err, response, body) {
+    res.send(body);
+  });
 });
