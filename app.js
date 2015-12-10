935f738206ec118a2b6717a3a28f789a2d38c2ce
Donald Brunais <donb1991@hotmail.com>
2015-12-10 09:07:45 -0800

    cookie session save for login

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var ejs = require('ejs');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
+var loginHelper = require('./middleware/loginHelper');
 var db = require('./models');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(session({
+  maxAge: 36000000,
+  secret: 'adfx',
+  name: 'onYourLeft'
+}));
+app.use(loginHelper);
 
 require('./controllers');
 
--- controllers/spotify.js
+++ controllers/spotify.js
@@ -1,6 +1,8 @@
 var queryString = require('querystring');
 var request = require('request');
+var loginHelper = require('../middleware/loginHelper');
 var stateKey = 'spotify_auth_state';
+var db = require('../models');
 
 
 app.get('/login', function(req, res) {
@@ -17,12 +19,15 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
+app.get('/callback', function(req, res) {
   var code = req.query.code || null;
   var state = req.query.state || null;
   var storedState = req.cookies ? req.cookies[stateKey] : null;
-
   if(state === null || state != storedState) {
     res.redirect('/#' + queryString.stringify({error: 'state_mismatch'}));
   } else {
@@ -35,15 +40,33 @@ app.get('/callback', function(req, res) {
         grant_type: 'authorization_code'
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(process.env.SPOTIFYID+ ':' + process.env.SPOTIFYSECRET).toString('base64'))
+        'Authorization': 'Basic ' + (new Buffer(process.env.SPOTIFYID + ':' + process.env.SPOTIFYSECRET).toString('base64'))
       },
       json: true
     };
 
     request.post(authOptions, function(error, response, body) {
       if(!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        var refresh_token = body.refresh_token;
+        var newUser = {};
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
 
-        res.redirect('/');
+        newUser.spotifyUserId = '';
+        newUser.access_token = body.access_token;
+        newUser.refresh_token = body.refresh_token;
+
+        request.get(options, function(error, response, body) {
+          newUser.spotifyUserId = body.id;
+          db.User.findOneAndUpdate({spotifyUserId: newUser.spotifyUserId}, newUser, {upsert: true}, function(err, data) {
+            req.login(newUser.spotifyUserId);
+            res.redirect('/');
+          });
+        });
       } else {
         res.redirect('/#' +
           queryString.stringify({
@@ -54,8 +77,26 @@ app.get('/callback', function(req, res) {
   }
 });
 
-app.post('/playlist', function(req, post) {
-
+app.get('/playlist', function(req, res) {
+  // var options = {
+  //   url: 'https://api.spotify.com/v1/users/donb91/playlists',
+  //   method: "POST",  
+  //   headers: {
+  //     'Authorization': 'Bearer ' + access_token,
+  //     'Content-Type': 'application/json',
+  //     "Accept": "application/json"
+  //   },
+  //   body: {
+  //     'name': "A NEW PLAYLIST ALL",
+  //     "public":false
+  //   },
+  //   json: true
+  // };
+  console.log(res);
+  res.send('res');
+  // request.post(options, function(error, response, body) {
+  //   console.log(error, body);
+  // });
 });
 
 function createRandomString(length) {
