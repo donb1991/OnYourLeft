6192427d030afa62c88300bc7c491b699a3bef73
Donald Brunais <donb1991@hotmail.com>
2015-12-14 13:43:06 -0800

    now can log out and rerender nav

--- public/nav.js
+++ public/nav.js
@@ -1,4 +1,9 @@
 var Nav = React.createClass({
+  getInitialState: function() {
+    return {
+      login: false
+    };
+  },
   login: function() {
     var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-read-private';
     var url = 'https://accounts.spotify.com/authorize?';
@@ -6,7 +11,8 @@ var Nav = React.createClass({
           response_type: 'code',
           client_id: '3985f789131b42f68a5dcebd5ae1b9cd',
           scope: scope,
-          redirect_uri: 'http://localhost:3000/callback'
+          redirect_uri: 'http://localhost:3000/callback',
+          show_dialog: true
         };
     var query = [];
     for (var i in params) {
@@ -25,8 +31,34 @@ var Nav = React.createClass({
       'Spotify',
       'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
     );
+   var loginWindowClosed = setInterval(() => {
+   if (loginWindow !== null) {
+      if (loginWindow.login) {
+        clearInterval(loginWindowClosed);
+        this.setState({login: true});
+       }
+     }
+   }, 1000);
+  },
+  logout: function() {
+    this.setState({login: false})
   },
   render: function() {
+    var menu;
+    if(this.state.login){
+      menu = <ul className="menu">
+        <li><a href="#">Build a Playlist</a></li>
+        <li><a href="#">View all Playlist</a></li>
+        <li><a href="#">Your Playlist</a></li>
+        <li><a href="#" onClick={this.logout}>Logout</a></li>
+      </ul>
+    } else {
+      menu = <ul className="menu">
+        <li><a href="#">Build a Playlist</a></li>
+        <li><a href="#">View all Playlist</a></li>
+        <li><a href="#" id='login' onClick={this.login}>Login</a></li>
+      </ul>
+    }
     return <nav className="top-bar" style={{"marginBottom": "30px"}}>
       <div className="top-bar-left">
         <ul className="menu">
@@ -34,12 +66,7 @@ var Nav = React.createClass({
         </ul>
       </div>
       <div className="top-bar-right">
-        <ul className="menu">
-          <li><a href="#">Build a Playlist</a></li>
-          <li><a href="#">View all Playlist</a></li>
-          <li><a href="#">Your Playlist</a></li>
-          <li><a href="#" id='login' onClick={this.login}>Login</a></li>
-        </ul>
+        {menu}
       </div>
     </nav>
   }
